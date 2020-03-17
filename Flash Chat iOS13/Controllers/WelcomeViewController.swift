eeb8112930662b9cad2b053b43efeabd07316eb6
Mohammad Athania <mohammadathania@Mohammads-MacBook-Pro.local>
2020-03-17 10:35:37 +0530

    Register & Login

---
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -0,0 +1,33 @@
+//
+//  WelcomeViewController.swift
+//  Flash Chat iOS13
+//
+//  Created by Angela Yu on 21/10/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//s
+
+import UIKit
+import CLTypingLabel
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: CLTypingLabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        titleLabel.text = "⚡️FlashChat"
+    //    var index = 0.0
+      //  titleLabel.text = ""
+        //let titleText = "⚡️FlashChat"
+      //  for letter in titleText{
+        //    Timer.scheduledTimer(withTimeInterval: 0.1 * index, repeats: false) { (timer) in
+          //      self.titleLabel.text?.append(letter)
+
+            //}
+            //index += 1
+        }
+       
+    }
+    
+
+
